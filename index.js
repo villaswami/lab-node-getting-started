5f0ee1c28fa9013e1aafc5b70688d86eeb615c74
villaswami <88075285+villaswami@users.noreply.github.com>
2022-04-14 10:10:07 +0530

    node

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 class SortedList {
   constructor() {
-  
+       this.items= [],
+       this.length = 0
   }
-  add() {
-   
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>(a-b))
+    this.length++;
   }
-  get() {
-
+  get(pos) {
+    if(this.items.indexOf(pos) == -1){
+      throw new Error('OutOfBounds')
+    }
+    else{
+      return(this.items.indexOf(pos))
+    }
   }
-  max() {
-
+  max()  {
+    if (this.items.length > 0) {
+      return Math.max(...this.items)
+    }else{
+      throw new Error('EmptySortedList')
+    }
   }
   min() {
-
+    if (this.items.length > 0) {
+      return Math.min(...this.items)
+    }else{
+      throw new Error('EmptySortedList')
+    }
   }
   avg() {
-    
-   
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList')
+    }else{
+      return this.sum() / this.items.length
+    }
   }
 
   sum() {
-  
+    if (this.items.length === 0) {
+      return 0
+    }else{
+      return this.items.reduce((a,b)=> a + b)
+    }
   }
+  };
   
-};
-
-module.exports = SortedList;
+  module.exports = SortedList;
